185aefc1943c45d21c824b04401d8662fe3c47e9
Igor Chernyshov <igorchernyshov@yandex.ru>
2022-12-04 11:51:30 +0300

    hw7 Books Spring Data Jpa

--- hw07-books-spring-data-jpa/src/main/java/ru/otus/books/repositories/BookRepository.java
+++ hw07-books-spring-data-jpa/src/main/java/ru/otus/books/repositories/BookRepository.java
@@ -5,12 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.books.domain.Book;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     @EntityGraph(value = "Book.AuthorGenre")
     List<Book> findAll();
-    Optional<Book> findById(long id);
-    Book save(Book book);
-    void deleteById(long id);
 }
