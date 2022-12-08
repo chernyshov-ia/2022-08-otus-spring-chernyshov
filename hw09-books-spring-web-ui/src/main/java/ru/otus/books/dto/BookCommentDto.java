f8c1974d24b8afd54067df5c082eae0dbc194e74
Igor Chernyshov <igorchernyshov@yandex.ru>
2022-12-08 10:52:24 +0300

    hw09 Spring Web UI

---
+++ hw09-books-spring-web-ui/src/main/java/ru/otus/books/dto/BookCommentDto.java
@@ -0,0 +1,17 @@
+package ru.otus.books.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.otus.books.domain.BookComment;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookCommentDto {
+    private Long id;
+    private String text;
+    public static BookCommentDto fromDomainObject(BookComment comment) {
+        return new BookCommentDto(comment.getId(), comment.getText());
+    }
+}
